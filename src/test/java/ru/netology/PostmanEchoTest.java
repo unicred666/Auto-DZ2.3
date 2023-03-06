5360518cd9e82f844a8b22749aeeb18e3e22beb9
Admin <admin@MacBook-Pro-Admin.local>
2023-03-06 22:26:54 +0300

    first commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Data"));
+                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
+    }
+}
